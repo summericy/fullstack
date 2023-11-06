be79c390674a14df495030ae765caaa72a117e19
summericy <671412430@qq.com>
2023-11-07 01:12:55 +0800

    chore: add routes env

---
+++ server/routes/Posts.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const { Posts } = require("../models")
+router.get("/", async(req, res) => {
+    const listPosts = await Posts.findAll()
+    res.json(listPosts)
+})
+
+router.post("/", async(req, res) => {
+    const post = req.body
+    await Posts.create(post)
+    res.json(post)
+})
+
+
+
+
+
+
+
+module.exports = router
